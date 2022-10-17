6a8b0a1ffa3946bbf9e20e116aa38ae3d443b0ee
Bryce Kowalczyk <bryce@lpadesign.com>
2022-10-17 00:51:32 -0400

    fix query

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -22,7 +22,7 @@ const styles = {
 }
 
 export const blogListQuery = graphql`
-  query blogListQuery($skip: Int!, $limit: Int!) {
+  query blogListQuery($skip: Int!) {
     allMarkdownRemark(
       sort: { order: DESC, fields: [frontmatter___date] }
       filter: { frontmatter: { template: { eq: "blog-post" } } }
